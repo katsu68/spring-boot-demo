d186d4ecf606f2f82b9d5b82f99db8a51da35f8a
masaru <katsuya@192.168.33.1>
2017-10-15 10:32:45 +0900

    URLパラメータのパターンを追加

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,20 @@ public class HelloController {
     @RequestMapping(value="/")
     private String hello(){
         return "Hello world!!!";
+    }
+
+    @RequestMapping(value="/rest/{path}")
+    private String rest(@PathVariable String path){
+        return "rest path";
+    }
+
+    @RequestMapping(value="/requireParam")
+    private String requireParam(@RequestParam String param){
+        return "require param => " + param;
+    }
+
+    @RequestMapping(value="/optinalParam")
+    private String optinalParam(@RequestParam(name = "param", required = false) Optional<String> param){
+        return "optinal param => " + param;
     }
 }
